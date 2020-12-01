f6e773f07d2dba673f57a14e40e5a7015d6d1790
Chris Jewell <chrisjohnjewell@gmail.com>
2020-12-01 17:14:48 +1100

    Day 1 part 2

--- lib/aoc2020/day_one.rb
+++ lib/aoc2020/day_one.rb
@@ -4,13 +4,26 @@ module Aoc2020
   module DayOne
     class << self
       def part_one(input)
-        numbers = input.chomp.split("\n").map(&method(:Integer))
+        first, second = numbers(input).combination(2).find { |x, y| x + y == 2020 }
+        first * second
+      end
 
-        first, second = numbers.combination(2).find do |(x, y)|
-          x + y == 2020
+      def part_two(input)
+        first, second, third = numbers(input).combination(3).find do |(x, y, z)|
+          x + y + z == 2020
         end
 
-        first * second
+        first * second * third
+      end
+
+      def part_n(input, take, total)
+        numbers(input).combination(take).find { |list| list.sum == total }&.reduce(:*)
+      end
+
+      private
+
+      def numbers(input)
+        input.chomp.split("\n").map(&method(:Integer))
       end
     end
   end
