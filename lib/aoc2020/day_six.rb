e152b4fdee9523f09530c31ed399640da43c0202
Chris Jewell <chrisjohnjewell@gmail.com>
2020-12-06 16:48:36 +1100

    Day 6 OO

--- lib/aoc2020/day_six.rb
+++ lib/aoc2020/day_six.rb
@@ -15,6 +15,18 @@ module Aoc2020
         end.sum
       end
 
+      def party_one(input)
+        input.chomp.split("\n\n").map do |lines|
+          AnswerGroup.from_lines(lines).anyone_answered.length
+        end.sum
+      end
+
+      def party_two(input)
+        input.chomp.split("\n\n").map do |lines|
+          AnswerGroup.from_lines(lines).everyone_answered.length
+        end.sum
+      end
+
       def anyone(group)
         group.gsub("\n", '').chars.sort.uniq.count
       end
@@ -37,6 +49,70 @@ module Aoc2020
           base.size
         end
       end
+    end
+
+    class AnswerGroup
+      class << self
+        def from_lines(lines)
+          person_answers = lines.split("\n").map do |line|
+            PersonAnswers.from_line(line)
+          end
+
+          new(person_answers: person_answers)
+        end
+      end
+
+      def initialize(person_answers:)
+        @person_answers = person_answers
+      end
+
+      def anyone_answered
+        person_answers.reduce do |memo, person_answer|
+          memo.union(person_answer)
+        end.answers
+      end
+
+      def everyone_answered
+        person_answers.reduce do |memo, person_answer|
+          memo.intersection(person_answer)
+        end.answers
+      end
+
+      private
+
+      attr_reader :person_answers
+    end
+
+    class PersonAnswers
+      include Enumerable
+
+      class << self
+        def from_line(line)
+          answers = line.chars.reduce(Set.new) do |acc, answer|
+            acc.add(answer)
+          end
+
+          new(answers: answers)
+        end
+      end
+
+      def initialize(answers:)
+        @answers = answers
+      end
+
+      def each(*args, &block)
+        answers.each(*args, &block)
+      end
+
+      def union(other_answers)
+        PersonAnswers.new(answers: answers.union(other_answers.answers))
+      end
+
+      def intersection(other_answers)
+        PersonAnswers.new(answers: answers.intersection(other_answers.answers))
+      end
+
+      attr_reader :answers
     end
   end
 end
